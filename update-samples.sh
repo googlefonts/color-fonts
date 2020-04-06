f78992ebcc6cd3e651f9301e9b024806e93d12a0
rsheeter <rsheeter@google.com>
2020-04-05 23:45:07 -0700

    build a twemoji sample too

--- update-samples.sh
+++ update-samples.sh
@@ -11,11 +11,25 @@ git submodule foreach git pull origin master
 
 source venv/bin/activate
 
+# don't have pip + versions currently; just grab latest
 pip install -e nanoemoji/
+pip install -e nanosvg/
 
 which nanoemoji  # sanity check
 
-# Handwriting
+# Noto Emoji Handwriting
+# https://rsheeter.github.io/android_fonts/emoji.html?q=u:270d
 nanoemoji --color_format colr_1 \
-	--output_file fonts/handwriting-colr_1.ttf \
-	$(find ../noto-emoji/svg -name 'emoji_u270d*.svg')
+	--output_file fonts/noto-handwriting-colr_1.ttf \
+	$(find font-srcs/noto-emoji/svg -name 'emoji_u270d*.svg')
+
+# Twemoji Smileys, these but twemoji:
+# https://rsheeter.github.io/android_fonts/emoji.html?q=note:smi
+nanoemoji --color_format colr_1 \
+	--output_file fonts/twemoji-smiley-colr_1.ttf \
+	font-srcs/twemoji/assets/svg/263a.svg \
+	font-srcs/twemoji/assets/svg/1f60{1,3,4,5,6,7,8,a,d,e,f}.svg \
+	font-srcs/twemoji/assets/svg/1f619.svg \
+	font-srcs/twemoji/assets/svg/1f63{8,a,b,c}.svg \
+	font-srcs/twemoji/assets/svg/1f642.svg \
+	font-srcs/twemoji/assets/svg/1f970.svg
