0be403308830a272a263c8e5c1b16347f979f95e
rsheeter <rsheeter@google.com>
2020-04-14 10:42:39 -0700

    licenses

--- update-samples.sh
+++ update-samples.sh
@@ -1,19 +1,33 @@
 #!/bin/bash -x
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 set -e
 
-git submodule init
-git submodule update
+# git submodule init
+# git submodule update
 
-git submodule foreach git pull origin master
+# git submodule foreach git pull origin master
 
-[ ! -d "venv" ] && python3 -m venv venv
+# [ ! -d "venv" ] && python3 -m venv venv
 
-source venv/bin/activate
+# source venv/bin/activate
 
-# # don't have pip + versions currently; just grab latest
-pip install -e nanoemoji/
-pip install -e nanosvg/
+# # # don't have pip + versions currently; just grab latest
+# pip install -e nanoemoji/
+# pip install -e nanosvg/
 
 which nanoemoji  # sanity check
 
