e46a7337fcc5eb40a600fa6ecbc540457d726a63
Cosimo Lupo <clupo@google.com>
2020-04-25 11:48:22 +0100

    remove git submodules, simply pip install -U nanoemoji in ./update-samples.sh

--- update-samples.sh
+++ update-samples.sh
@@ -16,18 +16,11 @@
 
 set -e
 
-# git submodule init
-# git submodule update
+[ ! -d "venv" ] && python3 -m venv venv
 
-# git submodule foreach git pull origin master
+source venv/bin/activate
 
-# [ ! -d "venv" ] && python3 -m venv venv
-
-# source venv/bin/activate
-
-# # # don't have pip + versions currently; just grab latest
-# pip install -e nanoemoji/
-# pip install -e nanosvg/
+pip install --upgrade nanoemoji
 
 which nanoemoji  # sanity check
 
