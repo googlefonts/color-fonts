5979e3980ed3d1c4ca27444fa98c327da2d0c469
rsheeter <rsheeter@google.com>
2020-06-29 16:37:11 -0700

    wider range of samples

--- rename.py
+++ rename.py
@@ -20,5 +20,4 @@ for f in os.listdir(dirpath):
   root, ext = os.path.splitext(f)
   old = os.path.join(dirpath, f)
   new = os.path.join(dirpath, '-'.join([f'{ord(c):x}' for c in root]) + ext)
-  print(f'{old} -> {new}')
   os.rename(old, new)
--- update-samples.sh
+++ update-samples.sh
@@ -22,9 +22,7 @@ function prebuild() {
 
 	source venv/bin/activate
 
-	# upgrade eagerly so that all dependencies are updated regardless of whether
-	# the currently installed version satisfies the requirement of the upgraded package.
-	pip install --upgrade --upgrade-strategy "eager" nanoemoji
+	pip install -r requirements.txt
 
 	which nanoemoji  # sanity check
 }
@@ -37,132 +35,71 @@ function compile_small_samples() {
 	twemoji_smiley_svgs="$twemoji_smiley_svgs $(echo font-srcs/twemoji/assets/svg/1f60{1,3,4,5,6,7,8,a,d,e,f}.svg)"
 	twemoji_smiley_svgs="$twemoji_smiley_svgs $(echo font-srcs/twemoji/assets/svg/1f63{8,a,b,c}.svg)"
 
-	# Noto Emoji Handwriting
-	# https://rsheeter.github.io/android_fonts/emoji.html?q=u:270d
-	nanoemoji --color_format glyf_colr_1 \
-		--output_dir fonts \
-		--output_file noto-handwriting-colr_1.ttf \
-		$noto_handwriting_svgs
-
-	nanoemoji --color_format svg \
-		--output_dir fonts \
-		--output_file noto-handwriting-svg.ttf \
-		$noto_handwriting_svgs
-
-	nanoemoji --color_format svgz \
-		--output_dir fonts \
-		--output_file noto-handwriting-svgz.ttf \
-		$noto_handwriting_svgs
-
-
-	# Twemoji Smileys, these but twemoji:
-	# https://rsheeter.github.io/android_fonts/emoji.html?q=note:smi
-	# Use COLRv0 since these don't use any gradiants
-	nanoemoji --color_format glyf_colr_0 \
-		--output_dir fonts \
-		--output_file twemoji-smiley-colr_0.ttf \
-		$twemoji_smiley_svgs
-
-	nanoemoji --color_format svg \
-		--output_dir fonts \
-		--output_file twemoji-smiley-svg.ttf \
-		$twemoji_smiley_svgs
-
-	nanoemoji --color_format svgz \
-		--output_dir fonts \
-		--output_file twemoji-smiley-svgz.ttf \
-		$twemoji_smiley_svgs
-
 	# SVG files from repo
 	# Name becomes the ligature to reach, e.g. radial.svg is a glyph activated by
 	# the string "radial"
 
 	# rename input files
-	tmp_dir=$(mktemp -d)
+	local tmp_dir=$(mktemp -d)
 	find font-srcs/samples -name '*.svg' -execdir cp {} "$tmp_dir/" ';'
-	ls $tmp_dir
 	python rename.py "$tmp_dir"
-	samples_svgs=$(find "$tmp_dir" -name '*.svg')
+	local samples_svgs=$(find "$tmp_dir" -name '*.svg')
 
-	nanoemoji --color_format glyf_colr_0 \
-		--output_dir fonts \
-		--output_file samples-colr_0.ttf \
-		$samples_svgs
+	# compile each sample set in each format
+	for fmt in glyf_colr_1 cff_colr_1 cff2_colr_1 picosvg picosvgz untouchedsvg untouchedsvgz; do
+		# Noto Emoji Handwriting
+		# https://rsheeter.github.io/android_fonts/emoji.html?q=u:270d
+		./compile.sh $fmt noto-handwriting $noto_handwriting_svgs &
 
-	nanoemoji --color_format glyf_colr_1 \
-		--output_dir fonts \
-		--output_file samples-colr_1.ttf \
-		$samples_svgs
+		# Twemoji Smileys, these but twemoji:
+		# https://rsheeter.github.io/android_fonts/emoji.html?q=note:smi
+		# Use COLRv0 since these don't use any gradiants
+		./compile.sh $fmt twemoji-smiley $twemoji_smiley_svgs &
 
-	nanoemoji --color_format svg \
-		--output_dir fonts \
-		--output_file samples-svg.ttf \
-		$samples_svgs
-
-	nanoemoji --color_format svgz \
-		--output_dir fonts \
-		--output_file samples-svgz.ttf \
-		$samples_svgs
+		# Sample files from repo
+		./compile.sh $fmt samples $samples_svgs &
+	done
 }
 
 function compile_noto_emoji() {
 	local fmt="$1"
-	time nanoemoji --color_format "$1" \
-		--output_dir fonts \
-		--output_file "noto-emoji-$1.ttf" \
-		$(find font-srcs/noto-emoji/svg -name '*.svg')
+	local svgs="$(find font-srcs/noto-emoji/svg -name '*.svg')"
+	./compile.sh $fmt noto-emoji $svgs &
 }
 
 function compile_twemoji() {
 	local fmt="$1"
-	time nanoemoji --color_format "$1" \
-		--output_dir fonts \
-		--output_file "twemoji-$1.ttf" \
-		$(find font-srcs/twemoji/assets/svg/ -name '*.svg')
+	local svgs="$(find font-srcs/twemoji/assets/svg/ -name '*.svg')"
+	./compile.sh $fmt twemoji $svgs &
 }
 
 function build() {
 	local target="$1"
+	local pids=""
 	case "$target" in
 	  small_samples)
 		prebuild
 		compile_small_samples
 	    ;;
-	  noto_emoji_colr)
-		prebuild
-		compile_noto_emoji glyf_colr_1
-		;;
-	  noto_emoji_svg)
-		prebuild
-		compile_noto_emoji svg
-		;;
-	  noto_emoji_svgz)
-		prebuild
-		compile_noto_emoji svgz
-		;;
-	  twemoji_colr)
-		prebuild
-		compile_twemoji glyf_colr_1
-	    ;;
-	  twemoji_colr)
-		prebuild
-		compile_twemoji svg
-	    ;;
-	  twemoji_colr)
-		prebuild
-		compile_twemoji svgz
-	    ;;
 	  twemoji)
 		prebuild
-		compile_twemoji svg
-		compile_twemoji svgz
+		compile_twemoji picosvg
+		compile_twemoji picosvgz
+		compile_twemoji untouchedsvg
+		compile_twemoji untouchedsvgz
 		compile_twemoji glyf_colr_1
+		compile_twemoji cff_colr_1
+		compile_twemoji cff2_colr_1
 	    ;;
 	  noto_emoji)
 		prebuild
-		compile_noto_emoji svg
-		compile_noto_emoji svgz
+		compile_noto_emoji picosvg
+		compile_noto_emoji picosvgz
+		compile_noto_emoji untouchedsvg
+		compile_noto_emoji untouchedsvgz
 		compile_noto_emoji glyf_colr_1
+		compile_noto_emoji cff_colr_1
+		compile_noto_emoji cff2_colr_1
 	    ;;
 	  *)  set +x
 	      echo "Invalid target $target"
@@ -170,22 +107,18 @@ function build() {
 	      exit 1
 	    ;;
 	esac
+
+	echo "blindly waiting for ALL compile.sh instances to exit :)"
+	wait $(pgrep -f compile.sh)
 }
 
 function usage() {
 	echo "Usage: -t target"
 	echo ""
 	echo "-t one of:"
-	echo "     all"
 	echo "     small_samples"
 	echo "     noto_emoji"
-	echo "     noto_emoji_colr"
-	echo "     noto_emoji_svg"
-	echo "     noto_emoji_svgz"
 	echo "     twemoji"
-	echo "     twemoji_colr"
-	echo "     twemoji_svg"
-	echo "     twemoji_svgz"
 }
 
 target=""
